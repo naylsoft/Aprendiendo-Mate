584f5ab61276b40e3eddc26e69981cef6670854b
naylsoft <naylsoft@gmail.com>
2016-11-10 06:53:24 -0500

    Tarea Academica 02
    by Nilton Trucios

---
+++ APRENDIENDO MATE/WindowsApplication11/APRENDIENDO MATE.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form1
+    Dim r1, r2, r3, r4 As Integer
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        r1 = Val(TextBox1.Text) + Val(TextBox2.Text)
+        TextBox7.Text = r1
+        r2 = Val(TextBox1.Text) - Val(TextBox2.Text)
+        TextBox4.Text = r2
+        r3 = Val(TextBox1.Text) * Val(TextBox2.Text)
+        TextBox6.Text = r3
+        r4 = Val(TextBox1.Text) / Val(TextBox2.Text)
+        TextBox5.Text = r4
+    End Sub
+End Class
